edd29d8e4fc3e8341643b3ff075aa1e6ad3a97f8
Joe Testa <jtesta@positronsecurity.com>
2020-10-01 08:53:50 -0400

    Bumped version.

--- version.h
+++ version.h
@@ -3,7 +3,7 @@
 
 #include "terminal_color.h"
 
-#define VERSION "v1.2"
+#define VERSION "v1.2-intel_skip_0"
 
 #define PRINT_PROJECT_HEADER() printf("\n%sRainbow Crackalack %s%s\nCopyright 2018-2020 Positron Security LLC <https://www.positronsecurity.com/>\n%sMake Rainbow Tables Great Again%s\n\n\n", WHITEB, VERSION, CLR, ITALICIZE, CLR);
 
